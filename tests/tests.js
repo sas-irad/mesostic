b82efc3b65feba2d25009ab413e0f4bd651a8257
J. Reuben Wetherbee <jrweth@gmail.com>
2015-05-08 15:19:45 -0400

    added a bunch of tests

--- tests/tests.js
+++ tests/tests.js
@@ -6,20 +6,25 @@
 QUnit.test('Startup Test', function(assert) {
     assert.ok(1 == "1", "QUnit Started Up");
 });
-
+//#########################createSourceArray testing ##############
 QUnit.test( "createSourceArray", function( assert ) {
     var testFunc = mesostic._createSourceArray;
     var sourceText;
     var sourceArray;
-    var expectedResult = ['Here', 'is', 'the', 'source', 'text'];
+    var expectedResult = ['here', 'is', 'the', 'source', 'text'];
     var options = {
         stripPunctuation: true,
     };
     
     //test vanilla example
-    sourceText = 'Here is the source text';
+    sourceText = 'here is the source text';
     sourceArray = testFunc(sourceText, options);
     assert.deepEqual(sourceArray, expectedResult, 'test vanilla')
+
+    //test capitalization example
+    sourceText = 'here Is the SouRce text';
+    sourceArray = testFunc(sourceText, options);
+    assert.deepEqual(sourceArray, expectedResult, 'test capitalization')
 
     //test some double spaces
     sourceText = ' Here   is     the source  text ';
@@ -48,15 +53,121 @@ QUnit.test( "createSourceArray", function( assert ) {
     assert.deepEqual(sourceArray, expectedResult, 'leave an apostrophe');
 
     //try leaving in punctuation
-    expectedResult = ['Here!', 'is,', 'Bob\'s,', '!text'];
-    sourceText = 'Here! is, Bob\'s, !text'];
+    options.stripPunctuation = false;
+    expectedResult = ['here!', 'is,', 'bob\'s,', '!text'];
+    sourceText = 'Here! is, Bob\'s, !text';
     sourceArray= testFunc(sourceText, options);
-    assert.deepEqual(sourceArray, expectedResult, 'leave punctuation');
+    assert.deepEqual(sourceArray, expectedResult, 'don\'t strip punctuation');
 });
 
 
+//#########################createSpineArray Testing ######################Y
+QUnit.test( "createSpineArray", function( assert ) {
+  
+    var testFunc = mesostic._createSpineArray;
+    var spineWord;
+    var spineArray;
+    var sourceArray;
+    var expectedResult;
+    var options = {
+        stripPunctuation: true,
+        rule: "basic",
+        spineLoops: 1
+    };
+
+    spineWord = 'slot';
+    sourceArray = [
+        'Here',       //0
+        'is',         //1 - 's' for basic, 50, 100
+        'a',          //2
+        'lovey',      //3 'l' for basic 
+        'silver',     //4 'l' for 50
+        'alive',      //5 'l' for  100
+        'source',     //6 'o' for basic, 50, 100
+        'array',      //7
+        'that',       //8 't' for basic, 50
+        'itches',     //9 't' for 100 (1 loop)
+        'into',       //10 't' for 100 (2 loops)
+        'the',        //11
+        'summer',     //12
+        'nightlife',  //13
+        'over',       //14
+        'cancun'      //15
+    ];
+    
+    //basic one loop
+    spineArray = testFunc(sourceArray, spineWord, options);
+    expectedResult = {parsed: true, spineArray: [
+        {spineLetter: 's', index:1, pre:1, post:0},//is
+        {spineLetter: 'l', index:3, pre:0, post:4},//lovely
+        {spineLetter: 'o', index:6, pre:1, post:4},//source
+        {spineLetter: 't', index:8, pre:0, post:3},//that
+    ]};
+    assert.deepEqual(spineArray, expectedResult, 'basic 1 loop');
+
+    //50% one loop
+    options.rule = "50";
+    spineArray = testFunc(sourceArray, spineWord, options);
+    expectedResult = {parsed: true, spineArray: [
+        {spineLetter: 's', index:1, pre:1, post:0},//is
+        {spineLetter: 'l', index:4, pre:2, post:3},//silver
+        {spineLetter: 'o', index:6, pre:1, post:4},//source
+        {spineLetter: 't', index:8, pre:0, post:3},//that
+    ]};
+    assert.deepEqual(spineArray, expectedResult, '50% 1 loop');
+    
+    //100% one loop
+    options.rule = "100";
+    spineArray = testFunc(sourceArray, spineWord, options);
+    expectedResult = {parsed: true, spineArray: [
+        {spineLetter: 's', index:1, pre:1, post:0},//is
+        {spineLetter: 'l', index:5, pre:1, post:3},//alive
+        {spineLetter: 'o', index:6, pre:1, post:4},//source
+        {spineLetter: 't', index:9, pre:1, post:4},//itches
+    ]};
+    assert.deepEqual(spineArray, expectedResult, '100% 1 loop');
+
+});
 
 
-QUnit.test( "hello test2", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+//#########################Parse Line Words Testing ######################Y
+QUnit.test('parseLineWords', function(assert) {
+    var testFunc = mesostic._parseLineWords;
+    
+    var parsed;
+    var spineArray = [
+        {spineLetter: 's', index:1, pre:1, post:0},//is
+        {spineLetter: 'l', index:5, pre:1, post:3},//alive
+        {spineLetter: 'o', index:6, pre:1, post:4},//source
+        {spineLetter: 't', index:9, pre:1, post:4},//itches
+    ];
+    var sourceArray = [
+        'here',       //0
+        'is',         //1 - 's' for basic, 50, 100
+        'a',          //2
+        'lovey',      //3 'l' for basic 
+        'silver',     //4 'l' for 50
+        'alive',      //5 'l' for  100
+        'source',     //6 'o' for basic, 50, 100
+        'array',      //7
+        'rattle',     //8 't' for basic, 50
+        'itches',     //9 't' for 100 (1 loop)
+        'into',       //10 't' for 100 (2 loops)
+        'the',        //11
+        'summer',     //12
+        'nightlife',  //13
+        'over',       //14
+        'cancun'      //15
+    ];
+    var options = {
+        rule: "basic",
+        wingTextSparsity: "normal"       
+    };
+
+    parsed = testFunc(2,3,spineArray, sourceArray, options);
+    assert.equal(parsed.startSpineLetter, 'O','check start spine letter');
+    assert.equal(parsed.endSpineLetter, 'T', 'check end spine letter');
+    assert.equal(parsed.preLineBreak.substr(0,4), 'urce', 'check preline break');
+
+    assert.equal(parsed.postLineBreak.substr(parsed.postLineBreak.length - 2), 'ra', 'check postline break');
 });
